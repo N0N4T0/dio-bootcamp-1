75cf98e01f2ff506dfbd5e4eef6b86304895e888
LUCIO Notebook <luciononato@hotmail.com>
2022-02-03 22:40:37 -0300

    Csharp

---
+++ CSharp/CSharp/Enum.cs
@@ -0,0 +1,11 @@
+namespace Enum
+{
+    enum Pessoas
+    {
+        Giovanna,
+        Julia,
+        João,
+        Gustavo,
+        Mariana
+    }
+}
---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Primeiro;
+using Classes;
+using Interface;
+
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa person = new Pessoa();
+
+            person.Nome = "None";
+            person.Idade = 23;
+            person.Estado = "HH";
+
+            var person2 = new Pessoa();
+            person2.Nome = "Done";
+            person2.Idade = 13;
+            person2.Estado = "AD";
+
+            var classe = new Classe();
+            var classe2 = new Segundo.Classe();
+
+            Console.WriteLine("Hello World");
+
+
+            Animal animal = new Animal();
+            animal.Nome = "Rex";
+            animal.NomeDono = "Rexona";
+            animal.Especie = "Cachorro";
+
+        }
+    }
+}
