771b4178b362401f5b6e4c1358c9fae7927fcea4
LUCIO Notebook <luciononato@hotmail.com>
2022-02-13 14:12:34 -0300

    Encapsulamento com validação

--- OrientacaoObjeto/ExemploPOO/Program.cs
+++ OrientacaoObjeto/ExemploPOO/Program.cs
@@ -5,12 +5,22 @@ namespace ExemploPOO{
     class Program{
         static void Main(string[] args)
         {
-            Pessoa p1= new Pessoa();
+            // Valores válidos
+            Retangulo r = new Retangulo();
+            r.DefinirMedidas(30, 30);
+            System.Console.WriteLine($"Área: {r.ObterArea()}");
+           
+            // Valores inválidos
+            Retangulo r2 = new Retangulo();
+            r2.DefinirMedidas(0, 30);
+            System.Console.WriteLine($"Área: {r2.ObterArea()}");
 
-            p1.Nome = "Bob";
-            p1.Idade = "20";
+            // Pessoa p1= new Pessoa();
 
-            p1.Apresentar();
+            // p1.Nome = "Bob";
+            // p1.Idade = "20";
+
+            // p1.Apresentar();
         }
     }
 }
